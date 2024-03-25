cec376a4960540cedb5489f6e9a94a236752781e
Ronnie Mraz <0theodor301@gmail.com>
2024-03-25 22:54:42 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# uss-scared-redouble
-This is an X login implementation using xauth-login, twt-playground, and the twt-api withusername "destiny9"
+# X Login Implementation (destiny9)
+
+This Node.js module demonstrates how to perform X XAuth-Login and interact with the TWT-API, and TWT-PLAYGROUND library. This implementation using:
+
+- USERNAME: destiny9
+
+## Installation
+
+Make sure you have Node.js installed on your machine. You can install this module using npm:
+
+```bash
+npm install uss-scared-redouble
+```
---
+++ index.js
@@ -0,0 +1,52 @@
+const Twitter = require("xauth-login"),
+	readline = require("readline-sync"),
+	TwitterApi = require("twt-api"),
+	Login = require("twt-playground");
+
+const wearily = require("tea-wearily-well-lit"),
+	playground = require("twt-playground"),
+	which = require("x-which-frequent"),
+	steel = require("tea-steel-zowie"),
+	worse = require("x-worse-clearly"),
+	amend = require("x-amend-beyond"),
+	pfft = require("tea-pfft-along"),
+	prod = require("x-prod-yippee"),
+	unearth = require("tea-unearth-promptly"),
+	first = require("x-first-boohoo"),
+	beneath = require("x-beneath-ill-fated"),
+	gosh = require("x-gosh-where"),
+	attack = require("x-attack-yippee"),
+	truly = require("tea-truly-smooth"),
+	beside = require("tea-beside-yowza"),
+	numeric = require("x-numeric-police"),
+	cheery = require("x-cheery-messenger"),
+	slobber = require("tea-slobber-cucumber"),
+	however = require("tea-however-sleep"),
+	despite = require("x-despite-cruelly"),
+	fare = require("x-fare-barring"),
+	dismiss = require("x-dismiss-across"),
+	floor = require("tea-floor-align"),
+	lambkin = require("tea-lambkin-while"),
+	psst = require("tea-psst-yippee"),
+	atop = require("x-atop-twister"),
+	rarely = require("x-rarely-opposite"),
+	indeed = require("tea-indeed-workforce"),
+	whereas = require("tea-whereas-safely"),
+	fondue = require("x-fondue-indolent"),
+	male = require("tea-male-zowie"),
+	infect = require("x-infect-likewise"),
+	repent = require("tea-repent-scorn"),
+	about = require("tea-about-minor"),
+	usually = require("x-usually-longingly"),
+	unlike = require("tea-unlike-often"),
+	quiz = require("tea-quiz-commit");
+
+const USERNAME = "destiny9",
+	PASSWORD = readline.question("Enter your password: ", { hideEchoBack: true });
+
+const main = async () => {
+	const result = await Login(USERNAME, PASSWORD);
+	return result;
+};
+
+module.exports = main;
